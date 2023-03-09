95983806938e84749835506836436dfe5a402adf
202310923psb <126744420+202310923psb@users.noreply.github.com>
2023-03-09 14:09:54 +0900

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -10,8 +10,8 @@ def get_circle_area(r):
 
 
 r = get_radius('넓이를 구하고자 하는 원의 반지름은?')
-get_circle_area(r)
-print('반지름 {}인 원의 넓이: 3.14 x {} x {} ='.format(r, r, r), area)
+result = get_circle_area(r)
+print('반지름 {}인 원의 넓이: 3.14 x {} x {} ='.format(r, r, r), result)
 t.sleep(500)
 
 
