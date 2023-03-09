a32b8175ec3c94a05a47d916c0740a6787f9b3b2
202310923psb <126744420+202310923psb@users.noreply.github.com>
2023-03-09 14:04:13 +0900

    Add files via upload

---
+++ hw1.py
@@ -0,0 +1,17 @@
+import time as t
+
+def get_radius(prompt):
+    r = int(input(prompt))
+    return r;
+
+def get_circle_area(r):
+    area = 3.14*r*r
+    print('반지름 {}인 원의 넓이: 3.14 x {} x {} ='.format(r, r, r), area)
+
+
+r = get_radius('넓이를 구하고자 하는 원의 반지름은?')
+get_circle_area(r)
+t.sleep(500)
+
+
+
