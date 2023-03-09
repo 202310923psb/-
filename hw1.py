a28daae3a9a0115c48c565f63d310e983fa6a4af
202310923psb <126744420+202310923psb@users.noreply.github.com>
2023-03-09 14:09:08 +0900

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -6,12 +6,12 @@ def get_radius(prompt):
 
 def get_circle_area(r):
     area = 3.14*r*r
-    return result;
+    return area;
 
 
 r = get_radius('넓이를 구하고자 하는 원의 반지름은?')
 get_circle_area(r)
-print('반지름 {}인 원의 넓이: 3.14 x {} x {} ='.format(r, r, r), result)
+print('반지름 {}인 원의 넓이: 3.14 x {} x {} ='.format(r, r, r), area)
 t.sleep(500)
 
 
