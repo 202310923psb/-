db26231e554ca6fc27d7e9c5455788e3fba86cac
202310923psb <126744420+202310923psb@users.noreply.github.com>
2023-03-30 14:44:11 +0900

    Update hw4.py

--- hw4.py
+++ hw4.py
@@ -1,5 +1,5 @@
 def rep_char(c, n):
-    print(str(c) * n)
+    print(str(c) * n + 4)
 
 def message(name):
     msg1 = f'Hello {name},'
@@ -10,7 +10,7 @@ def message(name):
         nstr = len(msg2)'''
     nstr = len(msg1) if (len(msg1) > len(msg2)) else len(msg2)
     rep_char('-', nstr)
-    print(f'{msg1}\n{msg2}')
+    print(f'  {msg1}  \n  {msg2}  ')
     rep_char('-', nstr)
 
 name = input('Input his/her name: ')
