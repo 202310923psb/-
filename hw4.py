1bb8663a69cfcb330c3531a8b6c223b94bd1d151
202310923psb <126744420+202310923psb@users.noreply.github.com>
2023-03-30 14:26:47 +0900

    Add files via upload

---
+++ hw4.py
@@ -0,0 +1,17 @@
+def rep_char(c, n):
+    print(str(c) * n)
+
+def message(name):
+    msg1 = f'Hello {name},'
+    msg2 = 'Welcome to Seoul.'
+    '''if len(msg1) > len(msg2):
+        nstr = len(msg1)
+    else:
+        nstr = len(msg2)'''
+    nstr = len(msg1) if (len(msg1) > len(msg2)) else len(msg2)
+    rep_char('-', nstr)
+    print(f'{msg1}\n{msg2}')
+    rep_char('-', nstr)
+
+name = input('Input his/her name: ')
+message(name)
