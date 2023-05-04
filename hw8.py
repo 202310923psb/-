8773b3e354eb32e82067b64abcbfc4345754d6c6
202310923psb <126744420+202310923psb@users.noreply.github.com>
2023-05-04 13:39:52 +0900

    Add files via upload

---
+++ hw8.py
@@ -0,0 +1,36 @@
+def buy(dic):
+    while True:
+        print('[구입]')
+        item = input('상품명? ')
+        if item == '':
+            print()
+            return False
+        num = int(input('수량은? '))
+        if item in dic:
+            dic[item] += num
+        else:
+            dic[item] = num
+        print(f'장바구니에 {item} {num}개가 담겼습니다.\n')
+
+def show(dic):
+    print(f'>>> 장바구니 보기:{dic}')
+    print()
+
+def find(dic):
+    if not shopping_bag == {}:
+        print('[검색]')
+        find_name = input('장바구니에서 확인하고자 하는 상품은? ')
+        if find_name not in shopping_bag:
+            print(f'장바구니에 {find_name}은(는) 없습니다.')
+            return None
+        print(f'{find_name}은(는) {shopping_bag[find_name]}개 담겨있습니다.')
+    else:
+        print('장바구니가 비어있습니다.')
+
+
+shopping_bag = {}
+while True:
+    if buy(shopping_bag) == False:
+        break
+show(shopping_bag)
+find(shopping_bag)
